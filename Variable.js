929544d711b570d7f86bacf9528c733db9a529f0
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2025-01-15 03:27:49 +0600

    Create Variable.js

---
+++ Variable.js
@@ -0,0 +1,25 @@
+// this is from the paid course of programiz hero
+// Variable-var
+// var name = value;
+// /semicolon might not be needed optional
+var weight=38;
+var money=1995
+// data types- 
+console.log(money);
+console.log(weight)
+
+var name="I am Zuthi"
+console.log(name)
+var bool=true
+console.log(bool)
+//  to see the type of variable
+console.log(typeof bool)
+// Alt+shift+down=copy
+// Alt+up=take the line up
+
+// tofix command= tofixed command deterines how many decimal number is wanted
+// the drawbacks of tofixed is that it converts the number into string
+var first =0.28
+var second=0.9
+console.log(first+second)
+console.log((first+second).toFixed(1))
