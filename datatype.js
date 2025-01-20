86a1501f4ec766952d321be3c41531857e3318c1
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2025-01-20 18:56:59 +0600

    Create datatype.js

---
+++ datatype.js
@@ -0,0 +1,22 @@
+//  conversion of datatype
+
+// float to integer- parseInt
+var num=5.5
+num=parseInt(num)
+console.log(num)
+
+// integer to float- parseFloat
+
+var num2= 10
+num2=parseFloat(num2)
+console.log(num2)
+
+// string to integer- Number
+
+var str='12'
+str=Number(str)
+console.log(str)
+
+// purpose of isNan=if a value is NaN (Not-a-Number). It returns true
+
+console.log(isNaN(str))
