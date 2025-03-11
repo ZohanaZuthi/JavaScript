7e9bfbddb4a25467a1b2b12f6f310cdcea78d6a8
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2025-03-11 23:49:43 +0600

    basics

---
+++ array.js
@@ -0,0 +1,15 @@
+// in js array need not be same type of variable 
+// datatype can be different
+// it is resizeable
+// array can be an element of an array
+const arr=[1,2,3,4,5,5]
+console.log(arr[2])
+const str=["shaktiman","naagraj",1,2,4]
+
+const arr2=new Array(1,2,3,4)
+console.log(arr2[0])
+arr2.push(6)
+arr2.push(7)
+console.log(arr2.length)
+arr2.pop()
+console.log(arr2.length)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-console.log(subs)
+naagraj
