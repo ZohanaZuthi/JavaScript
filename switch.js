95b2233f0560f0a9576716663d1148da3458d6cd
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2025-03-18 01:08:50 +0600

    basic

---
+++ switch.js
@@ -0,0 +1,11 @@
+const month =3
+switch(month)
+{
+    case 1: console.log("Its one"); break;
+    case 2: console.log("Its two"); break;
+    case 3: console.log("Its three"); break;
+    case 4: console.log("Its four"); break;
+    default: break;
+
+    
+}
